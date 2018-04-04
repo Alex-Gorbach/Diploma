42bc01bec23310cfbb1462b3d53c87a307dfe110
Alex-Gorbach <alex_gorbach@inbox.ru>
2018-04-04 03:19:21 +0300

    created DAL structure

---
+++ Diploma/DAL/EF/ApplicationContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using DAL.Entities;
+
+namespace DAL.EF
+{
+    public class ApplicationContext:IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationContext(string conectionString) : base(conectionString) { }
+
+        public DbSet<ClientProfile> ClientProfiles { get; set; }
+
+    }
+}
