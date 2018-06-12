eb4fbd3c09f08ed62ed2ed1fc225d408fba76088
Alex-Gorbach <alex_gorbach@inbox.ru>
2018-06-12 13:46:43 +0300

    last commit

--- Diploma/MyMenu.Parser/Core/Servise/DataServise.cs
+++ Diploma/MyMenu.Parser/Core/Servise/DataServise.cs
@@ -4,6 +4,7 @@ using MyMenu.DAL.Repositories;
 using System.Collections.Generic;
 using System.Data.Entity.Validation;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using WindowsFormsApp1.Core.Recepies;
 
@@ -12,16 +13,16 @@ namespace WindowsFormsApp1.Core.Servise
     public class DataServise : IDataService
     {
         IUnitOfWork Database { get; set; }
-        public DataServise(IUnitOfWork uow)
+        public DataServise( IUnitOfWork uow )
         {
             Database = uow;
         }
         public DataServise()
         {
-            Database = new IdentityUnitOfWork("YourMenuComtext");
+            Database = new IdentityUnitOfWork( "YourMenuComtext" );
         }
 
-        public async Task Create(ArborioModel arborioModel)
+        public async Task Create( ArborioModel arborioModel )
         {
             var recipe = new Recipe()
             {
@@ -33,23 +34,23 @@ namespace WindowsFormsApp1.Core.Servise
 
             RecipeProduct recipeProduct;
             Product productFind = null;
-            var result = Database.RecipeManager.GetRecipeByName(arborioModel.Name);
+            var result = Database.RecipeManager.GetRecipeByName( arborioModel.Name );
             if (result.Count == 0)
             {
                 try
                 {
-                    recipe = Database.RecipeManager.Create(recipe);
+                    recipe = Database.RecipeManager.Create( recipe );
                 }
                 catch (DbEntityValidationException e)
                 {
                     foreach (var eve in e.EntityValidationErrors)
                     {
-                        Debug.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
+                        Debug.WriteLine( "Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
+                            eve.Entry.Entity.GetType().Name, eve.Entry.State );
                         foreach (var ve in eve.ValidationErrors)
                         {
-                            Debug.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
-                                ve.PropertyName, ve.ErrorMessage);
+                            Debug.WriteLine( "- Property: \"{0}\", Error: \"{1}\"",
+                                ve.PropertyName, ve.ErrorMessage );
                         }
                     }
                     throw;
@@ -57,25 +58,31 @@ namespace WindowsFormsApp1.Core.Servise
 
                 for (int i = 0; i < arborioModel.Products.Count; i++)
                 {
-                    productFind = Database.ProductManager.FindProductByName(arborioModel.Products[i]);
+                    productFind = Database.ProductManager.FindProductByName( arborioModel.Products[i] );
                     if (productFind != null)
                     {
                         recipeProduct = new RecipeProduct()
                         {
                             RecipeId = recipe.RecipeId,
                             ProductId = productFind.ProductId,
-
                         };
-                        if (arborioModel.Units.Count >= i)
+                        var res = Database.RecipeProductManager.GetRecipeIdByProductId( productFind.ProductId );
+                        var recipeRes = res.Where( x => x.RecipeId == recipe.RecipeId );
+                        if (recipeRes.Count() == 0)
+                        {
                             recipeProduct.Number = arborioModel.Number[i];
+                            Database.RecipeProductManager.Create( recipeProduct );
+                        }
+                        else
+                        {
+                            var ds = 5;
+                        }
 
-                        Database.RecipeProductManager.Create(recipeProduct);
                     }
                     else
                     {
                         product.Name = arborioModel.Products[i];
-                        //product.Unit = arborioModel.Units[i];
-                        Database.ProductManager.Create(product);
+                        Database.ProductManager.Create( product );
 
                         recipeProduct = new RecipeProduct()
                         {
@@ -84,11 +91,11 @@ namespace WindowsFormsApp1.Core.Servise
 
                         };
                         recipeProduct.Number = arborioModel.Number[i];
-                        Database.RecipeProductManager.Create(recipeProduct);
+                        Database.RecipeProductManager.Create( recipeProduct );
                     }
                     await Database.SaveAsync();
                 }
-                
+
             }
 
         }
